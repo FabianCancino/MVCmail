47769cfd22c5bd6dee71ceb69d4e0df49ede9968
FabianCancino <fabian.cancino,a@gmail.com>
2021-07-09 01:01:26 -0400

    upload code

---
+++ MailProject/Controllers/ContactsController.cs
@@ -0,0 +1,136 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MailProject.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private ContactEntities1 db = new ContactEntities1();
+
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            return View(db.Contact.ToList());
+        }
+
+        // GET: Contacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contact.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nombre,email,mensaje,id")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contact.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contact.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "nombre,email,mensaje,id")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contact.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contact.Find(id);
+            db.Contact.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendEmail(string useremail, string subject, string body)
+        {
+            //get mail
+
+            WebMail.Send(useremail, subject, body, null, null, null, true, null, null, null, null, null, null);
+
+            return View();
+        }
+    }
+}
